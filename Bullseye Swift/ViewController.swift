65f3739356ad8a837ef5f6886e69b057caf7487b
benben <benben>
2014-11-04 20:36:00 -0600

    Initial Commit

---
+++ Bullseye Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bullseye Swift
+//
+//  Created by benjaminhallock@gmail.com on 11/4/14.
+//  Copyright (c) 2014 BEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
